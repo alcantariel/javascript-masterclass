34ed102646793e886cf0da8d913890c38f05c75e
Gabriel Alcântara <galcantaradev@gmail.com>
2022-04-16 14:15:41 -0300

    add exercise 12

---
+++ exercicios/12_exercicio /README.md
@@ -0,0 +1,44 @@
+# Exercício 12
+
+## Objetivo
+
+Utilize async/await para tratar as chamadas para as promises.
+
+<br>
+
+## Instruções
+
+Crie uma função utilizando async.
+
+Invoque cada uma das funções execute utilizando await incluindo o Promise.all.
+
+Envolva as chamadas em um bloco try/catch para tratar as exceções.
+
+<br>
+
+## Resultado
+
+```json
+[
+  {
+    "name": "Douglas Crockford",
+    "age": "62"
+  },
+  {
+    "name": "Linus Torvalds",
+    "age": "47"
+  },
+  {
+    "name": "Martin Fowler",
+    "age": "54"
+  }
+]
+```
+
+<br>
+
+## Conteúdo abordado neste exercício
+
+- Async/Await
+- Promises
+- try/catch
---
+++ exercicios/12_exercicio /index.mjs
@@ -0,0 +1,29 @@
+import { Database } from "./Database.mjs";
+
+(async function () {
+  try {
+    const database = new Database();
+
+    await database.execute(
+      "create table author (id number, name string, age number, city string, state string, country string)"
+    );
+
+    await Promise.all([
+      database.execute(
+        "insert into author (id, name, age) values (1, Douglas Crockford, 62)"
+      ),
+      database.execute(
+        "insert into author (id, name, age) values (2, Linus Torvalds, 47)"
+      ),
+      database.execute(
+        "insert into author (id, name, age) values (3, Martin Fowler, 54)"
+      ),
+    ]);
+
+    const result = await database.execute("select name, age from author");
+
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+})();
